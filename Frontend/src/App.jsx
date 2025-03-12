8527068e8a0c9adb56b1a739492880780a00e560
AliviaNandi <121783301+AliviaNandi@users.noreply.github.com>
2025-03-12 21:30:12 +0530

    User Login and SignUp

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,13 @@
+/* eslint-disable no-unused-vars */
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
 import UserProfile from './components/UserProfile.jsx/UserProfile'
 import Therapists from './components/Therapists/Therapists'
 import Booking from './components/Booking/Booking'
+import SignUp from './components/SignUp/SignUp'
+import OTPVerification from './components/OTPVerfication/OTPVerification'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,7 +16,9 @@ function App() {
     <>
      <Header/>
      <Booking/>
-     <Footer/>
+     <Footer/> 
+    <SignUp/>
+     <OTPVerification/>
     </>
   )
 }
