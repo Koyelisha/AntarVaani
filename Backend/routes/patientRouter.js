810874e6959e1f3b97d4514ef8a2ea5545c9df66
Koyelisha <koyelisha7@gmail.com>
2025-03-16 12:06:59 +0530

    patient route

--- Backend/routes/patientRouter.js
+++ Backend/routes/patientRouter.js
@@ -1,13 +1,9 @@
 const express = require("express")
 const patientModel = require("../models/patient-model")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-const nodemailer = require("nodemailer")
-const crypto = require("crypto")
-const { generateToken } = require("../utils/generateToken")
-
 const router = express.Router()
-const { signupPatient,verifyOTP,loginPatient } = require("../controllers/authController");
+const { signupPatient,verifyOTP,loginPatient } = require("../controllers/authController")
+const therapistModel = require("../models/therapist-model")
+
 
 router.get("/", (req, res) => {
     res.send("Hey!patient router working..")
@@ -18,5 +14,15 @@ router.post("/signup", signupPatient);
 router.post("/verify-otp",verifyOTP);
 
 router.post("/login",loginPatient);
+
+router.get("/showTherapists",async(req,res)=>{
+    try{
+        let therapists = await therapistModel.find();
+        return res.send(therapists)
+    }catch(err){
+        return res.send(err.message)
+    }
+    
+})
 
 module.exports = router;
